90f95a7afb9a984fbd9cced9f3e47b46ad729337
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-05-16 09:42:17 +0200

    status dot fix

--- src/components/landing/PageHeader.vue
+++ src/components/landing/PageHeader.vue
@@ -5,7 +5,7 @@
         <img src="../../assets/logo.png" alt="server logo" class="heading-logo">
         <h1 class="heading-title">NazwaSerwera.pl | Twój serwer Skyblock</h1>
         <div class="heading-server-status-wrapper">
-          <span class="pulse-animation pulse-dot-33"></span>
+          <span class="pulse-animation pulse-dot-20"></span>
           <p class="heading-server-status">Status serwera: Online [5/100]</p>
         </div>
       </div>
