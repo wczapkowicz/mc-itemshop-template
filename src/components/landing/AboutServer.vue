8f3cb8e3d1d5f40f9dd2cb27a2944b81732b87c9
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-05-17 11:11:34 +0200

    about section basic html

--- src/components/landing/AboutServer.vue
+++ src/components/landing/AboutServer.vue
@@ -1,8 +1,14 @@
 <template>
     <div id="about">
         <div class="container-fluid">
-            <div class="row">
-                
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-sm-12 col-md-6">
+                    <h1 class="about-heading">O serwerze</h1>
+                    <p class="about-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae assumenda possimus dolorem sint eius nihil aperiam neque consectetur blanditiis excepturi aliquam laboriosam fuga, sapiente atque animi ratione, optio iusto minima. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, quos dolorum. Cumque deleniti tenetur itaque dolorum enim cupiditate pariatur optio a nulla, culpa eaque sequi, sed sit dolorem quasi fugiat.</p>
+                </div>
+                <div class="col-sm-12 col-md-6">
+
+                </div>
             </div>
         </div>
     </div>
