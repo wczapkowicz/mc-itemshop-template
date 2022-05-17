17c35163b4ef6019dde8fb9b21e7de03ac1c40fb
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-05-17 11:15:19 +0200

    about section image

--- src/components/landing/AboutServer.vue
+++ src/components/landing/AboutServer.vue
@@ -6,8 +6,8 @@
                     <h1 class="about-heading">O serwerze</h1>
                     <p class="about-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae assumenda possimus dolorem sint eius nihil aperiam neque consectetur blanditiis excepturi aliquam laboriosam fuga, sapiente atque animi ratione, optio iusto minima. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, quos dolorum. Cumque deleniti tenetur itaque dolorum enim cupiditate pariatur optio a nulla, culpa eaque sequi, sed sit dolorem quasi fugiat.</p>
                 </div>
-                <div class="col-sm-12 col-md-6">
-
+                <div class="col-sm-12 col-md-6 d-flex justify-content-end">
+                    <img class="about-image" src="../../assets/about-render.png" alt="player render">
                 </div>
             </div>
         </div>
