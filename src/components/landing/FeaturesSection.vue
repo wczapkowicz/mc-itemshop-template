517f17701d6fe6635605b68690999fbbc6e14b0b
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-05-24 20:21:13 +0200

    features section

--- src/components/landing/FeaturesSection.vue
+++ src/components/landing/FeaturesSection.vue
@@ -1,11 +1,42 @@
 <template>
     <div id="features">
-        
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-12 col-md-6 col-lg-4 feature">
+                    <div class="feature-icon">
+                        <h3><i class="fas fa-star"></i></h3>
+                    </div>
+                    <h3 class="feature-heading">Lorem ipsum</h3>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                        provident maxime quisquam laboriosam</p>
+                </div>
+
+                <div class="col-sm-12 col-md-6 col-lg-4 feature">
+                    <div class="feature-icon">
+                        <h3><i class="fas fa-user"></i></h3>
+                    </div>
+                    <h3 class="feature-heading">Lorem ipsum</h3>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                        provident maxime quisquam laboriosam</p>
+                </div>
+
+                <div class="col-sm-12 col-md-6 col-lg-4 feature">
+                    <div class="feature-icon">
+                        <h3><i class="fas fa-wallet"></i></h3>
+                    </div>
+                    <h3 class="feature-heading">Lorem ipsum</h3>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                        provident maxime quisquam laboriosam</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
+  export default {
+    name: 'FeaturesSection',
+  }
 </script>
 
 <style lang="scss" scoped>
