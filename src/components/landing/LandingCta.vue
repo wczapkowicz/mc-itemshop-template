9e864aad6c492406280ce3131515f9ac89c034aa
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-05-25 21:16:03 +0200

    CTA section content

--- src/components/landing/LandingCta.vue
+++ src/components/landing/LandingCta.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="cta">
-
+        <div class="container-fluid">
+            <h1>Dołącz do 10 graczy online!</h1>
+            <a href="#" class="btn btn-lg btn-outline-primary heading-button">Zagraj teraz!</a>
+        </div>
     </div>
 </template>
 
