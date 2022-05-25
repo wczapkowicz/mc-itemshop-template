9af3c208ebafa3923742c8d0fe1b4dcd637aa417
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-05-25 23:46:34 +0200

    contact section

--- src/components/landing/ContactPage.vue
+++ src/components/landing/ContactPage.vue
@@ -1,6 +1,26 @@
 <template>
     <div id="contact">
-        
+        <div class="container-fluid">
+            <h1 class="contact-title">Skontaktuj się z nami!</h1>
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    <div class="form-wrapper">
+                        <form>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="name" placeholder="Imie" required>
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="email" placeholder="Adres e-mail" required>
+                            </div>
+                            <div class="mb-3">
+                                <textarea class="form-control" id="textarea-contact" rows="5" placeholder="Wiadomość" required></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-outline-primary sign-in-button btn-lg">Wyślij</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
